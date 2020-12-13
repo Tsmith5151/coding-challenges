f6c02365460dca992b54dbc3f089711f6ece99cf
Trace Smith <tsmith5151@gmail.com>
2020-12-13 14:02:21 -0600

    update scripts

--- src/sort.py
+++ src/sort.py
@@ -1,3 +1,5 @@
+from typing import List
+
 def sortColors(nums):
     """
     :type nums: List[int]
@@ -24,3 +26,48 @@ def restoreString(s, indices):
     encode = {i: str(s[idx]) for idx, i in enumerate(indices)}
     string = "".join([encode[i] for i in range(0, len(indices))])
     return string
+
+
+class QuickSort:
+    """  Quicksort Implementation"""
+
+    def __init__(self):
+        self.threshold = 1
+
+    def sort(self, arr:List[int]):
+        self.helper(arr, low=0, high=len(arr)-1)
+        return arr
+
+    def helper(self, arr, low, high):
+        if low < high:
+            p = self.partition(arr, low, high)
+            self.helper(arr, low, p - 1)
+            self.helper(arr, p + 1, high)
+
+    def get_pivot(self, arr, low, high):
+        """ get median pivot value"""
+        mid = (high + low) // 2
+        s = sorted([arr[low], arr[mid], arr[high]])
+        if s[1] == arr[low]:
+            return low
+        elif s[1] == arr[mid]:
+            return mid
+        return high
+
+    def partition(self, arr, low, high):
+        pivotIndex = self.get_pivot(arr, low, high)
+        pivotValue = arr[pivotIndex]
+        arr[pivotIndex], arr[low] = arr[low], arr[pivotIndex]
+        border = low
+
+        # iterate over array
+        for i in range(low, high+1):
+            if arr[i] < pivotValue:
+                border += 1
+                #swap with border
+                arr[i], arr[border] = arr[border], arr[i]
+        
+        # swap pivot value into the border position
+        arr[low], arr[border] = arr[border], arr[low]
+
+        return border
---
+++ src/stacks.py
@@ -0,0 +1,11 @@
+def isValidSerialization(preorder):
+    preorder, stack = preorder.split(","), []
+    for node in preorder:
+        print(stack)
+        while stack and node == stack[-1] == "#":
+            stack.pop()
+            if not stack: 
+                return False
+            stack.pop()
+        stack.append(node)
+    return stack == ["#"]
