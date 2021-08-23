c4c735063857ef4673d99945ccfabcf57e0e65e3
Trace Smith <tsmith5151@gmail.com>
2021-08-23 10:15:56 -0500

    update scripts

---
+++ scripts/array_three_sum.py
@@ -0,0 +1,66 @@
+""" 
+3Sum
+
+Reference: https://leetcode.com/problems/3sum/
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
+
+Input: nums = []
+Output: []
+Example 3:
+
+Input: nums = [0]
+Output: []
+
+Solution: pointers  
+Time complexity: O(nlogn) + O(n^2) = O(n^2)
+Space complexity: O(1) or O(n)
+"""
+
+def threeSum(nums):
+	"""
+	:type nums: List[int]
+	:rtype: List[List[int]]
+	"""
+	results = []
+	# sort the input array to avoid duplicates
+	nums.sort()
+	
+	# iterate through the array
+	for idx, i in enumerate(nums):
+		# to avoid using the same value twice and continue to next iteration
+		if idx > 0 and i == nums[idx-1]:
+			continue
+		# apply two sum approach: set the left and right pointers
+		left = idx + 1
+		right = len(nums) - 1
+		while left < right:
+			threeSum = i + nums[left] + nums[right]
+			# threshold is target 0
+			if threeSum  > 0:
+				right -= 1
+			# threshold target
+   			elif threeSum < 0:
+				left += 1
+			# if the sum is 0, add to results
+   			else:
+				results.append([i, nums[left], nums[right]])
+				left += 1
+				# update the left most pointer now
+				# update the left and check if the value is the same 
+				while nums[left] == nums[left -1] and left < right:
+					left += 1
+
+	return results
+
+if __name__ == '__main__':
+    nums = [-1,0,1,2,-1,-4]
+    print(threeSum(nums))
--- scripts/array_two_sum.py
+++ scripts/array_two_sum.py
@@ -26,7 +26,32 @@ def twoSum(nums, target):
             seen[nums[i]] = i
     return seen
 
+def twoSum2(nums,target):
+    """
+    :type nums: List[int]
+    :type target: int 
 
-nums = [2, 7, 11, 15]
-target = 9
-twoSum(nums, target)
+    Approach: using two pointers
+    Time complexity: O(n)
+    """
+    left_pointer = 0
+    right_pointer = len(nums) - 1
+    while left_pointer <= right_pointer:
+        # compute sum of current left and right pointers
+        curSum = nums[right_pointer] + nums[left_pointer]
+        # if sum is greater than target, move right pointer left 
+        if curSum > target: 
+            right_pointer -= 1
+        # if sum is less than target, move left pointer right 
+        if curSum < target: 
+            left_pointer += 1
+        # if sum is equal to target, return results 
+        else:
+            return [left_pointer + 1, right_pointer + 1]
+    return [] 
+
+if __name__ == '__main__':
+    nums = [2, 7, 11, 15]
+    target = 9
+    # print(twoSum(nums, target))
+    print(twoSum2(nums, target))
