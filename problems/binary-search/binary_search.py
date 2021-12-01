5b0dda9a13654b61bb0735980a48140b2ab279f5
Trace Smith <tsmith5151@gmail.com>
2021-12-01 09:02:34 -0600

    update scripts

--- problems/binary-search/binary_search.py
+++ problems/binary-search/binary_search.py
@@ -1,26 +1,23 @@
 """
-## Binary Search
+Binary Search
 
-Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
-You must write an algorithm with O(log n) runtime complexity.
+Reference: https://leetcode.com/problems/binary-search/
 
-Link: https://leetcode.com/problems/binary-search/
+Given an array of integers nums which is sorted in ascending order, and an
+integer target, write a function to search target in nums. If target exists,
+then return its index. Otherwise, return -1. 
+
+Note: You must write an algorithm with O(log n) runtime complexity.
 
 Example 1:
-
-```
 Input: nums = [-1,0,3,5,9,12], target = 9
 Output: 4
 Explanation: 9 exists in nums and its index is 4
-```
 
 Example 2:
-
-```
 Input: nums = [-1,0,3,5,9,12], target = 2
 Output: -1
 Explanation: 2 does not exist in nums so return -1
-```
 
 Steps:
     - Compare x with the middle element.
@@ -32,11 +29,13 @@ Steps:
         - So we apply the algorithm for the left half.
 """
 
-from typing import List
 
-
-def binary_search(nums: List[int], target: int):
-    """Implement Binary Search"""
+def binary_search(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
     left = 0
     right = len(nums) - 1
     while left < right:
@@ -51,6 +50,7 @@ def binary_search(nums: List[int], target: int):
     return -1
 
 
-nums = [-1, 0, 3, 5, 9, 12]
-target = 9
-print(binary_search(nums, target))
+if __name__ == "__main__":
+    nums = [-1, 0, 3, 5, 9, 12]
+    target = 9
+    print(binary_search(nums, target))
